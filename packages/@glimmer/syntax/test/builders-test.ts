db81e63c467728f189c43fc44a56a3dd55ffcac0
Cyrille David <cyrille@qonto.eu>
2021-05-17 13:53:08 +0200

    Add a test to describe a synthetic node

--- packages/@glimmer/syntax/test/builders-test.ts
+++ packages/@glimmer/syntax/test/builders-test.ts
@@ -10,3 +10,11 @@ QUnit.test('element uses comments as loc when comments is not an array', functio
 
   astEqual(actual, expected);
 });
+
+QUnit.test('a node is synthetic when it is built with a builder', function (assert) {
+  let node = b.text('hello');
+  let syntheticNode = b.text('hello', b.loc(1, 1, 1, 1));
+
+  assert.equal(node.loc.source, '(synthetic)');
+  assert.equal(syntheticNode.loc.source, '(synthetic)');
+});
